a182154bf4fa605b4ce5665149aba9c882cbd297
JC <jhegs.knightdev@gmail.com>
2018-10-01 12:31:55 +0800

    Set up retrofit client

--- app/src/main/java/org/ayannah/jcc/doyouknowit/network/NetworkClient.java
+++ app/src/main/java/org/ayannah/jcc/doyouknowit/network/NetworkClient.java
@@ -1,4 +1,18 @@
 package org.ayannah.jcc.doyouknowit.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NetworkClient {
+
+    private static String BASE_URL = "http://jservice.io/";
+    private static Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = builder.build();
+
+    public static <S> S buildConnection(Class<S> apiType){
+        return retrofit.create(apiType);
+    }
 }
