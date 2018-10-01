bc81dec0baf9924da57778e9f4010360e71c7f47
JC <jhegs.knightdev@gmail.com>
2018-10-01 12:17:54 +0800

    Added packages for organization

--- app/src/main/java/org/ayannah/jcc/doyouknowit/activity/SplashScreen.java
+++ app/src/main/java/org/ayannah/jcc/doyouknowit/activity/SplashScreen.java
@@ -1,7 +1,9 @@
-package org.ayannah.jcc.doyouknowit;
+package org.ayannah.jcc.doyouknowit.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.ayannah.jcc.doyouknowit.R;
 
 public class SplashScreen extends AppCompatActivity {
 
