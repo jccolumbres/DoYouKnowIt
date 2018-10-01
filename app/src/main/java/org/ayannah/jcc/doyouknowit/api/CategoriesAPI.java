b728dca0ac21ec25ab7402d390f4831eb8e56fd5
JC <jhegs.knightdev@gmail.com>
2018-10-01 14:08:49 +0800

    Set up interface CategoriesAPI

--- app/src/main/java/org/ayannah/jcc/doyouknowit/api/CategoriesAPI.java
+++ app/src/main/java/org/ayannah/jcc/doyouknowit/api/CategoriesAPI.java
@@ -1,4 +1,15 @@
 package org.ayannah.jcc.doyouknowit.api;
 
+import org.ayannah.jcc.doyouknowit.models.Categories;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface CategoriesAPI {
+
+
+    @GET("/api/categories/?count={value}")
+    Call<Categories> getCategories (@Path("value") String value);
+
 }
