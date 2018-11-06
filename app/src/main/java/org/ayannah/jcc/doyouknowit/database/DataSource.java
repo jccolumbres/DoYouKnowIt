3ed4c634422b7bf99a68ee7afa0aad706387879e
Jigs <jhegs.knightdev@gmail.com>
2018-11-06 15:29:03 +0800

    Added open and close methods to DBHelper

--- app/src/main/java/org/ayannah/jcc/doyouknowit/database/DataSource.java
+++ app/src/main/java/org/ayannah/jcc/doyouknowit/database/DataSource.java
@@ -1,4 +1,28 @@
 package org.ayannah.jcc.doyouknowit.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 public class DataSource {
+
+    public static Context mContext;
+    public static SQLiteOpenHelper mDbHelper;
+    public static SQLiteDatabase mDatabase;
+
+    public DataSource(Context context) {
+        this.mContext = context;
+        mDbHelper = new DBHelper(mContext);
+        mDatabase = mDbHelper.getWritableDatabase();
+    }
+
+    public void open() {
+        mDatabase = mDbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        mDbHelper.close();
+    }
+
+
 }
