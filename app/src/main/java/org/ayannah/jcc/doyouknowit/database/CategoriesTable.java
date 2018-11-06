c3425b7d50b5dbd70b3640d44db2e89c324c1e08
Jigs <jhegs.knightdev@gmail.com>
2018-11-06 15:02:37 +0800

    Set up dbhelper class and tables class

--- app/src/main/java/org/ayannah/jcc/doyouknowit/database/CategoriesTable.java
+++ app/src/main/java/org/ayannah/jcc/doyouknowit/database/CategoriesTable.java
@@ -1,4 +1,18 @@
 package org.ayannah.jcc.doyouknowit.database;
 
 public class CategoriesTable {
+    private static final String TABLE_CATEGORIES = "categories";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_CATEGORYID = "categoryId";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_COUNT = "count";
+
+    public static final String SQL_CREATE = "CREATE TABLE " + TABLE_CATEGORIES + "(" +
+            COLUMN_ID + " TEXT PRIMARY KEY," +
+            COLUMN_CATEGORYID + " TEXT," +
+            COLUMN_TITLE + " TEXT," +
+            COLUMN_COUNT + " INTEGER" + ");";
+
+    public static final String SQL_DELETE = "DROP TABLE " + TABLE_CATEGORIES;
+
 }
--- app/src/main/java/org/ayannah/jcc/doyouknowit/database/DBHelper.java
+++ app/src/main/java/org/ayannah/jcc/doyouknowit/database/DBHelper.java
@@ -1,4 +1,27 @@
 package org.ayannah.jcc.doyouknowit.database;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import org.ayannah.jcc.doyouknowit.models.Categories;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "categories.db";
+    private static final int DB_VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CategoriesTable.SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(CategoriesTable.SQL_DELETE);
+        onCreate(db);
+    }
 }
